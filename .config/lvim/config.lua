75cd07124cbaeae78a98ff8ec85b8ddb0f29684e
Sayuri Nekomiya <BBH@awsl.rip>
2024-07-28 20:17:26 +1000

    Close buffer

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -20,7 +20,7 @@ lvim.plugins = {
         cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
         build = "cd app && npm install",
         init = function()
-          vim.g.mkdp_filetypes = { "markdown" }
+            vim.g.mkdp_filetypes = { "markdown" }
         end,
         ft = { "markdown" },
     },
@@ -42,15 +42,35 @@ lvim.autocommands = {
                 vim.api.nvim_set_hl(0, "NvimTreeFolderIcon", { fg = "#7aa2f7" })
                 vim.api.nvim_set_hl(0, "DapStoppedLinehl", { bg = "#555530" })
 
-                vim.api.nvim_set_hl(0, 'DapBreakpoint', { fg='#993939' })
-                vim.api.nvim_set_hl(0, 'DapLogPoint', { fg='#98c379' })
-                vim.api.nvim_set_hl(0, 'DapBreakpointCondition', { fg='#efaf61' })
+                vim.api.nvim_set_hl(0, 'DapBreakpoint', { fg = '#993939' })
+                vim.api.nvim_set_hl(0, 'DapLogPoint', { fg = '#98c379' })
+                vim.api.nvim_set_hl(0, 'DapBreakpointCondition', { fg = '#efaf61' })
 
-                lvim.builtin.dap.breakpoint = { text = lvim.icons.ui.Circle, texthl = "DapBreakpoint", linehl = "", numhl = "" }
-                lvim.builtin.dap.breakpoint_rejected = { text = lvim.icons.diagnostics.BoldError, texthl = "Error", linehl = "", numhl = "" }
-                lvim.builtin.dap.stopped = { text = lvim.icons.ui.BoldArrowRight, texthl = "Error", linehl = "DapStoppedLinehl", numhl = "" }
-                vim.fn.sign_define("DapBreakpointCondition", { text = lvim.icons.ui.Circle, texthl = "DapBreakpointCondition", linehl = "", numhl = "" })
-                vim.fn.sign_define("DapLogPoint", { text = lvim.icons.ui.Circle, texthl = "DapLogPoint", linehl = "", numhl = "" })
+                lvim.builtin.dap.breakpoint = {
+                    text = lvim.icons.ui.Circle,
+                    texthl = "DapBreakpoint",
+                    linehl = "",
+                    numhl =
+                    ""
+                }
+                lvim.builtin.dap.breakpoint_rejected = {
+                    text = lvim.icons.diagnostics.BoldError,
+                    texthl = "Error",
+                    linehl =
+                    "",
+                    numhl = ""
+                }
+                lvim.builtin.dap.stopped = {
+                    text = lvim.icons.ui.BoldArrowRight,
+                    texthl = "Error",
+                    linehl =
+                    "DapStoppedLinehl",
+                    numhl = ""
+                }
+                vim.fn.sign_define("DapBreakpointCondition",
+                    { text = lvim.icons.ui.Circle, texthl = "DapBreakpointCondition", linehl = "", numhl = "" })
+                vim.fn.sign_define("DapLogPoint",
+                    { text = lvim.icons.ui.Circle, texthl = "DapLogPoint", linehl = "", numhl = "" })
             end,
         },
     },
@@ -79,18 +99,20 @@ lvim.keys.normal_mode['<F9>'] = "<Cmd>lua require'dap'.continue()<CR>"
 lvim.keys.normal_mode['<F10>'] = "<Cmd>lua require'dap'.step_out()<CR>"
 lvim.keys.normal_mode['<F12>'] = "<Cmd>lua require'dap'.terminate()<CR>"
 
-lvim.builtin.which_key.mappings['dn'] = {"<Cmd>lua require('dap-python').test_method()<CR>", "Debug Python Method"}
-lvim.builtin.which_key.mappings['df'] = {"<Cmd>lua require('dap-python').test_class()<CR>", "Debug Python Class"}
+lvim.builtin.which_key.mappings['dn'] = { "<Cmd>lua require('dap-python').test_method()<CR>", "Debug Python Method" }
+lvim.builtin.which_key.mappings['df'] = { "<Cmd>lua require('dap-python').test_class()<CR>", "Debug Python Class" }
 lvim.builtin.which_key.vmappings['d'] = {
     name = "Debug",
-    s = {"<ESC><Cmd>lua require('dap-python').debug_selection()<CR>", "Debug Selected Python Code"}
+    s = { "<ESC><Cmd>lua require('dap-python').debug_selection()<CR>", "Debug Selected Python Code" }
 }
 lvim.builtin.which_key.mappings['m'] = {
     name = "MarkdownPreview",
-    s = { "<Cmd>MarkdownPreview<CR>", "MarkdownPreview"},
-    x = { "<Cmd>MarkdownPreviewStop<CR>", "MarkdownPreviewStop"},
-    t = { "<Cmd>MarkdownPreviewToggle<CR>", "MarkdownPreviewToggle"}
+    s = { "<Cmd>MarkdownPreview<CR>", "MarkdownPreview" },
+    x = { "<Cmd>MarkdownPreviewStop<CR>", "MarkdownPreviewStop" },
+    t = { "<Cmd>MarkdownPreviewToggle<CR>", "MarkdownPreviewToggle" }
 }
+
+lvim.builtin.which_key.mappings['bc'] = { "<Cmd>bd<CR>", "Close Buffer" }
 
 local dap, dapui = require("dap"), require("dapui")
 
@@ -110,7 +132,7 @@ lvim.builtin.treesitter.highlight = {
     disable = {
         "latex"
     },
-    additional_vim_regex_highlighting = {"python"}
+    additional_vim_regex_highlighting = { "python" }
 }
 
 package.path = package.path .. ";/Users/socular/.config/lvim/?.lua"
