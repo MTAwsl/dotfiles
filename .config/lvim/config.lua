b67760f9bdadf8f652ca95953e093917037bc847
Sayuri Nekomiya <BBH@awsl.rip>
2025-02-28 20:42:21 +1100

    Clang-format

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -130,30 +130,31 @@ end
 -- Function to check if a floating dialog exists and if not
 -- then check for diagnostics under the cursor
 function OpenDiagnosticIfNoFloat()
-for _, winid in pairs(vim.api.nvim_tabpage_list_wins(0)) do
-  if vim.api.nvim_win_get_config(winid).zindex then
-    return
-  end
+    for _, winid in pairs(vim.api.nvim_tabpage_list_wins(0)) do
+        if vim.api.nvim_win_get_config(winid).zindex then
+            return
+        end
+    end
+    -- THIS IS FOR BUILTIN LSP
+    vim.diagnostic.open_float(0, {
+        scope = "cursor",
+        focusable = false,
+        close_events = {
+            "CursorMoved",
+            "CursorMovedI",
+            "BufHidden",
+            "InsertCharPre",
+            "WinLeave",
+        },
+    })
 end
--- THIS IS FOR BUILTIN LSP
-vim.diagnostic.open_float(0, {
-  scope = "cursor",
-  focusable = false,
-  close_events = {
-    "CursorMoved",
-    "CursorMovedI",
-    "BufHidden",
-    "InsertCharPre",
-    "WinLeave",
-  },
-})
-end
+
 -- Show diagnostics under the cursor when holding position
 vim.api.nvim_create_augroup("lsp_diagnostics_hold", { clear = true })
 vim.api.nvim_create_autocmd({ "CursorHold" }, {
-pattern = "*",
-command = "lua OpenDiagnosticIfNoFloat()",
-group = "lsp_diagnostics_hold",
+    pattern = "*",
+    command = "lua OpenDiagnosticIfNoFloat()",
+    group = "lsp_diagnostics_hold",
 })
 
 lvim.builtin.indentlines.active = false
@@ -167,3 +168,16 @@ lvim.builtin.treesitter.highlight = {
 
 package.path = package.path .. ";/Users/socular/.config/lvim/?.lua"
 require("markdown-config")
+
+-- Clang-format
+local formatters = require "lvim.lsp.null-ls.formatters"
+
+formatters.setup {
+    { command = "clang-format",
+        extra_args = { "--style", "file:/Users/socular/.clang-format",  },
+    },
+}
+
+lvim.lsp.null_ls.setup.on_init = function(new_client, _)
+  new_client.offset_encoding = "utf-8"
+end
