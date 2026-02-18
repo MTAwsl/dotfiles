08acc373226e4b0171c7264b4673352f6823cc2e
Sayuri Nekomiya <bbh@awsl.rip>
2026-02-19 01:08:55 +1100

    Flake update; XDG Portal Fix

--- users/yuri/home/xdg.nix
+++ users/yuri/home/xdg.nix
@@ -88,10 +88,9 @@
         portal = {
           enable = true;
           extraPortals = with pkgs; [
-            xdg-desktop-portal
-            xdg-desktop-portal-gtk
+            xdg-desktop-portal-gnome
           ];
-          config.common.default = [ "gtk" ];
+          config.common.default = [ "gnome" ];
         };
       };
     };
